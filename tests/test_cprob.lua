c8c803b8beeee776c1543be95bd3d7c1b5dcb913
Dan Horgan <daniel.horgan@deepmind.com>
2013-10-21 17:54:32 +0100

    Test gamma corner cases

--- tests/test_cprob.lua
+++ tests/test_cprob.lua
@@ -112,6 +112,18 @@ end
 function callTests.test_lgam()
     local x = 0.5
     tester:assert(cephes.lgam(x))
+    tester:assertError(cephes.lgam(-3))
+    tester:asserteq(cephes.lgam(-3 + 1e-350), -math.huge)
+    tester:asserteq(cephes.lgam(-3 - 1e-350), math.huge)
+    tester:assertError(cephes.lgam(-4))
+    tester:asserteq(cephes.lgam(-4 + 1e-350), math.huge)
+    tester:asserteq(cephes.lgam(-4 - 1e-350), -math.huge)
+    tester:assertError(cephes.lgam(0))
+    tester:asserteq(cephes.lgam(0 + 1e-350), math.huge)
+    tester:asserteq(cephes.lgam(0 - 1e-350), -math.huge)
+    tester:asserteq(cephes.lgam(1e600), math.huge)
+    tester:assert(cephes.isnan(cephes.lgam(-math.huge)))
+    tester:asserteq(cephes.lgam(math.huge), math.huge)
 end
 
 -- Test simple calls for gdtr
