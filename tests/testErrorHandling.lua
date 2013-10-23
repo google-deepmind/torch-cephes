36f7a4c5c26d1f68d2d3196b102ff9079a21fdf4
Julien Cornebise <julien.cornebise@gmail.com>
2013-10-23 16:34:49 +0100

    Allow user to control the error reporting
    By default, do not issue any error. Can increase to warning,
    and to error.

--- tests/testErrorHandling.lua
+++ tests/testErrorHandling.lua
@@ -10,7 +10,12 @@ end
 
 function errTest.testError()
     local df = 4
+    local previousLevel = cephes.getErrorLevel()
+    cephes.setErrorLevel(0)
+    tester:assert(function() cephes.chdtr(-1,1) end)
+    cephes.setErrorLevel(1)
     tester:assertError(function() cephes.chdtr(-1,1) end)
+    cephes.setErrorLevel(previousLevel)
 end
 
 tester:add(errTest)
