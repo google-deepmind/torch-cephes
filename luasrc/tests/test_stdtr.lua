a5d0a0cfae1c9619c141d60d4443b9436c9e7919
Julien Cornebise <julien@cornebise.com>
2013-12-02 18:43:29 +0000

    Add test for cdflib's version of stdtr

--- luasrc/tests/test_stdtr.lua
+++ luasrc/tests/test_stdtr.lua
@@ -4,10 +4,16 @@ local myTests = {}
 local tester = torch.Tester()
 
 
-function myTests.testHuge()
+function myTests.testCephesHuge()
     tester:asserteq(cephes.stdtr(10, math.huge), 1, 'CDF(math.huge) != 1')
     tester:asserteq(cephes.stdtr(10, 1e155), 1, 'CDF(1e155) != 1')
 end
+
+function myTests.testCdflibHuge()
+    tester:asserteq(cephes.ffi.cdft1_wrap(10, math.huge), 1, 'CDF(math.huge) != 1')
+    tester:asserteq(cephes.ffi.cdft1_wrap(10, 1e155), 1, 'CDF(1e155) != 1')
+end
+
 
 tester:add(myTests)
 tester:run()
